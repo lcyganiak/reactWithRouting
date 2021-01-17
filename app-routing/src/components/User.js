f9e2339607fd844b82b80e0ccf530a0f5d929392
lukasz.cyganiak <lukaszcyganiak4@gmail.com>
2021-01-17 15:45:27 +0100

    add remove user

--- app-routing/src/components/User.js
+++ app-routing/src/components/User.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 
 class User extends Component {
 
-
+  removeUser = (user) => {
+    this.props.delete(user.key)
+  }
   render() {
     const list = this.props.userList.map(item => {
-      return <li key={item.key}>{item.name}</li>
+      return <li onClick={() => this.removeUser(item)} key={item.key}>{item.name}</li>
     })
     return (
       <ul>
